4d781e12eca2399e0a4d239f955c865688e339ea
Samueltired <124218801+Samueltired@users.noreply.github.com>
2023-02-02 21:36:46 -0300

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,32 @@
+public class helloWorld {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // variaves 
+     int a = 10;
+     int b = 5;
+     int x = 0;
+     int f =45;
+     
+     
+     //função
+     x=a+b;
+     a=b+f;
+     
+
+
+        System.out.println("ola mundo");
+        System.out.println(x);
+        System.out.println(a);
+        
+
+
+
+    }
+
+}
+
+
+
+
